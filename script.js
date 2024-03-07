492cefa0461ab814264fe5f6a69e4bfbf19ecc7f
99narii <122865149+99narii@users.noreply.github.com>
2024-03-07 13:25:44 +0900

    login page

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+//로그인
+
+const memberLoginBtn = document.querySelector('.member-login-btn');
+const buySearchBtn = document.querySelector('.buy-search-btn');
+const loginMemberDiv = document.querySelector('.login-member');
+const buySearchFormDiv = document.querySelector('.buy-search-form');
+
+// 가입 회원 버튼 클릭 이벤트
+memberLoginBtn.addEventListener('click', function() {
+    loginMemberDiv.style.display = 'flex'; 
+    buySearchFormDiv.style.display = 'none';
+    memberLoginBtn.classList.add('active-login-btn');
+    buySearchBtn.classList.remove('active-login-btn'); 
+});
+
+buySearchBtn.addEventListener('click', function() {
+    loginMemberDiv.style.display = 'none'; 
+    buySearchFormDiv.style.display = 'flex'; 
+    buySearchBtn.classList.add('active-login-btn');
+    memberLoginBtn.classList.remove('active-login-btn'); 
+});
